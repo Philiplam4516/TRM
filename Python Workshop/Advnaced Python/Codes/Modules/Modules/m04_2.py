87e4a01e557517181e5b76d08e547b1e5636016a
Philiplam4516 <Philiplam4516@hotmail.com>
2021-10-29 17:20:30 +1100

    Delete Python Workshop/Advnaced Python/Codes directory

--- Python Workshop/Advnaced Python/Codes/Modules/Modules/m04_2.py
+++
@@ -1,7 +0,0 @@
-'''
-Write a program to get a  circle radius from terminal, 
-and then compute the area of the circle by a function 
-'''
-PI = 3.14
-def compute_circle_area(radius):
-  return radius*radius*PI
